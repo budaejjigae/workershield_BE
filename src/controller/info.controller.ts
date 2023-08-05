1af99af5781e9c7ab63149566889d613c6ef9589
12xii <ur12xii@gmail.com>
2023-08-06 08:20:25 +0900

    ✨ :: feat :: getArticle

--- src/controller/info.controller.ts
+++ src/controller/info.controller.ts
@@ -1,8 +1,9 @@
-import { getArticleList } from '@src/service/info.service';
+import { getArticle, getArticleList } from '@src/service/info.service';
 import * as express from 'express';
 
 const router = express.Router();
 
+router.get('/:id', getArticle);
 router.get('/', getArticleList);
 
 export { router };
--- src/service/info.service.ts
+++ src/service/info.service.ts
@@ -1,14 +1,12 @@
 import { AppDataSource } from "@src/database/Database";
 import { Article } from "@src/database/entity/article.entity";
 import { Company } from "@src/database/entity/company.entity";
-import { User } from "@src/database/entity/user.entity";
 import { validateAccess } from "@src/utils/middleware/validate.middle";
 import { config } from "dotenv";
 import { Request, Response } from "express";
 
 config();
 
-const userRepo = AppDataSource.getRepository(User);
 const companyRepo = AppDataSource.getRepository(Company);
 const articleRepo = AppDataSource.getRepository(Article);
 
@@ -48,6 +46,30 @@ const getArticleList = async (req: Request, res: Response): Promise<object> => {
     })
 }
 
+const getArticle = async (req: Request, res: Response): Promise<object> => {
+    const thisUser = await validateAccess(req.headers.authorization!);
+    const { id } = req.params;
+    const articleID = Number(id);
+
+    const thisArticle = await articleRepo.findOneBy({ articleID });
+
+    if (!thisUser) return res.status(404).json({
+        errCode: 404,
+        errMsg: "존재하지 않는 유저"
+    })
+    if (!thisArticle) return res.status(404).json({
+        errCode: 404,
+        errMsg: "존재하지 않는 아티클"
+    })
+
+    return res.status(200).json({
+        data: thisArticle,
+        statusCode: 200,
+        statusMsg: "아티클 조회 성공"
+    })
+}
+
 export {
-    getArticleList
+    getArticleList,
+    getArticle
 }
